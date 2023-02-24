c9f0276d2fda539622dcef73f26db88a104ccb56
alsgurdk22 <39902920+alsgurdk22@users.noreply.github.com>
2023-02-24 17:49:07 +0900

    Update app.js

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,18 @@
-alert('hi');
+const url = window.location.host
+const socket = new WebSocket(`ws://${url}`);
+
+socket.addEventListener('open', () => {
+    console.log('Connected to Server 😀');
+});
+
+socket.addEventListener('message', (message) => {
+    console.log('New message: ', message.data);
+});
+
+socket.addEventListener('close', () => {
+    console.log('Disconnected from Server ❌');
+});
+
+setTimeout(() => {
+    socket.send('hello from the browser!');
+}, 1000);
