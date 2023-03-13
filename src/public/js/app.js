971d187b89cca1f4326b21d1a8e485591f888071
정민혁 <seaweed@talentbank.co.kr>
2023-03-13 16:21:30 +0900

    메시지 보내기, 닉네임 만들기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,19 +2,67 @@ const socket = io();
 
 const welcome = document.getElementById("welcome");
 const form = document.querySelector("form");
+const room = document.getElementById("room");
 
-function backendDone(msg) {
-    console.log(`msg: `, msg);
+room.hidden = true;
+
+let roomName;
+
+function addMessage(message) {
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerHTML = message;
+    ul.appendChild(li);
+}
+
+function handleMessageSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("#msg input");
+    const value = input.value;
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${value}`);
+    });
+    input.value = "";
+}
+
+function handleNickNameSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("#name input");
+    socket.emit("nickname", input.value);
+    document.querySelector("h2").textContent = input.value;
+    room.querySelector("#name").hidden = true;
+}
+
+function showRoom() {
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.textContent = `Room Name: ${roomName}`;
+    const msgForm = room.querySelector("#msg");
+    const nickNameForm = room.querySelector("#name");
+    msgForm.addEventListener("submit", handleMessageSubmit);
+    nickNameForm.addEventListener("submit", handleNickNameSubmit);
 }
 
 function handleRoomSubmit(event) {
     event.preventDefault();
     const input = form.querySelector("input");
-    socket.emit("enter_room", input.value, backendDone);
+    socket.emit("enter_room", input.value, showRoom);
+    roomName = input.value;
     input.value = "";
 }
 
 form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", (user) => {
+    addMessage(`${user} joined!!`);
+});
+
+socket.on("bye", (left) => {
+    addMessage(`${left} left ㅠㅠ!`);
+});
+
+socket.on("new_message", addMessage);
 // websocket 사용 시
 // const messageList = document.querySelector('ul');
 // const nickForm = document.querySelector('#nick');
